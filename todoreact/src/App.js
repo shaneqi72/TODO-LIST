ac50e5d2ae38d95020e3546c6939841298cd4f9d
shaneqi72 <69065798+shaneqi72@users.noreply.github.com>
2020-11-28 08:47:05 +1000

    adding funtion filter

--- todoreact/src/App.js
+++ todoreact/src/App.js
@@ -16,14 +16,23 @@ function App() {
         return res.json();
       })
       .then((todos) => {
-        setTodos(todos);
+        switch (status) {
+          case 'completed':
+            setTodos(todos.filter((todo) => todo.completed === true));
+            break;
+          case 'uncompleted':
+            setTodos(todos.filter((todo) => todo.completed === false));
+            break;
+          default:
+            setTodos(todos);
+            break
+        }
       });
-  }
+  };
 
   useEffect(() => {
     getTodos()
-  }, []);
-
+  }, [status]);
 
 
   return (
